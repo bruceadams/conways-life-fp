0f9ded76a516001efae874921ade5c7c24c3d845
Bruce Adams <bruce.adams@acm.org>
2012-09-12 21:08:53 -0400

    Update to clojure 1.4

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,3 +1,3 @@
 (defproject life "1.0.0-SNAPSHOT"
   :description "Simple implementation of Conway's Game of Life"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
